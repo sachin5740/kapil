9f70b877a2e9f1831cd0f5193677404d805dfe9a
sachin5740 <58850946+sachin5740@users.noreply.github.com>
2020-07-17 22:10:32 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='capitalize',
+      version='1.0',
+      # list folders, not files
+      packages=['capitalize',
+                'capitalize.test'],
+      scripts=['capitalize/bin/cap_script.py'],
+      package_data={'capitalize': ['data/cap_data.txt']},
+      )
